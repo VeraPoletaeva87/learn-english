c815e68e24a9092117156de3506b751aeb1e1221
Vera Poletaeva <poletaeva.vera.vladimirovna@gmail.com>
2024-02-16 13:43:55 -0500

    insert task styles

--- src/data/sentences.ts
+++ src/data/sentences.ts
@@ -8,13 +8,13 @@ export const sentences = [
     { id: 2,
         options: ['happy', 'sad', 'excited', 'hungry'], 
         answer: 1,
-        part1: 'My cat was very',
+        part1: 'My dog was very',
         part2: 'because meatball was too small'
    },
     { id: 3,
         options: ['happy', 'sad', 'excited', 'hungry'], 
         answer: 3,
-        part1: 'My cat was very',
+        part1: 'My kitten was very',
         part2: 'because he have no second breakfast today'
      },
   ];
