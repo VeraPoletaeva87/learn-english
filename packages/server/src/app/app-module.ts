2efb4e1d825840664e498195ef6ace038de648b0
Artem Ganev <a.a.ganev@gmail.com>
2024-02-20 13:03:30 -0500

    Add server package

---
+++ packages/server/src/app/app-module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get('DB_URI');
+        return { uri: uri };
+      },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'client', 'dist'),
+      exclude: ['/api/(.*)'],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class AppModule {}
